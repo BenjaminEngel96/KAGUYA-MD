9c0d660a9e79e4e5f74396c8b14213d8c3d634fa
Official <102423761+BenjaminEngel96@users.noreply.github.com>
2022-04-01 11:09:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,9 +22,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6281285866502', '6281398003097']
-global.botname = 'BAYUBOT-MD'
-global.ownername = 'BAYU OFC'
+global.owner = ['6289529688364', '62895329513790']
+global.botname = 'BenjaminEngel'
+global.ownername = 'BenjaminEngel'
 global.packname = 'BAYUBOT-MD'
 global.author = 'CREATED BY BAYU OFC'
 global.sessionName = 'session'
